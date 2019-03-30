aecfffd4ac95da50d2d82ee1e8a0da0cf0ed8123
andreas divaris <dredivaris@gmail.com>
2019-03-30 16:50:16 -0700

    Recipes latest

--- src/components/recipes/recipes.tsx
+++ src/components/recipes/recipes.tsx
@@ -1,15 +1,66 @@
+import beverages from '@/queries/beverages';
+import { allApiBeverages } from '@/queries/types/allApiBeverages';
+import { List, Typography } from '@material-ui/core';
 import * as React from 'react';
+import { Query } from 'react-apollo';
 
 // ----------------------------------------------------------------------------
 
 // Say hello from GraphQL, along with a HackerNews feed fetched by GraphQL
-const Recipes: React.FC = (props) => {
-  console.log('props are', props);
+const Recipes: React.FC = () => {
+  // console.log('props are', props);
 
   return (
-    <>
-      <h2>Recipe list loaded!</h2>
-    </>
+    <Query<allApiBeverages> query={beverages}>
+      {(result) => {
+        if (result.data && result.data.allApiBeverages) {
+          console.log('our results are...', result.data.allApiBeverages.nodes);
+          const beverages = result.data.allApiBeverages.nodes;
+          beverages.map((bev) => {
+            console.log('in bev, ', bev);
+          });
+        }
+
+        // Any errors? Say so!
+        if (result.error) {
+          return <h1>Error retrieving units! &mdash; {result.error}</h1>;
+        }
+
+        // If the data is still loading, return with a basic
+        // message to alert the user
+        if (result.loading) {
+          return <h1>Loading units ...</h1>;
+        }
+        // const name = result.data!.allApiUnits!.nodes[0]!.name;
+        // const {
+        //   data: {
+        //     allApiUnits: { nodes },
+        //   },
+        // } = result;
+
+        // Otherwise, we have data to work with... map over it with a
+        // bullet-point list
+
+        return (
+          <>
+            <List>
+              {result.data &&
+                result.data.allApiBeverages &&
+                result.data.allApiBeverages.nodes.map((bev) => {
+                  return <Typography>{bev!.name}</Typography>;
+                })}
+              {/*{result.data!.hn.topStories.map(story => (*/}
+              {/*  <Story key={story.id}>*/}
+              {/*    <a href={story.url} target="_blank">*/}
+              {/*      {story.title}*/}
+              {/*    </a>*/}
+              {/*  </Story>*/}
+              {/*))}*/}
+            </List>
+          </>
+        );
+      }}
+    </Query>
   );
 };
 
