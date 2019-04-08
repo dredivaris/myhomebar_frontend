3fc354c7564ebf7592f20e88a425a1562a47bd33
andreas divaris <dredivaris@gmail.com>
2019-04-07 17:04:28 -0700

    Update apollo setup file to handle schema stitching

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -4,32 +4,54 @@
 // IMPORTS
 
 /* NPM */
-import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
-import { ApolloClient } from "apollo-client";
-import { ApolloLink, split } from "apollo-link";
-import { onError } from "apollo-link-error";
-import { HttpLink } from "apollo-link-http";
-import { WebSocketLink } from "apollo-link-ws";
-import { getMainDefinition } from "apollo-utilities";
-import { SubscriptionClient } from "subscriptions-transport-ws";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink, split } from 'apollo-link';
+import { onError } from 'apollo-link-error';
+import { HttpLink } from 'apollo-link-http';
+import { SchemaLink } from 'apollo-link-schema';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { introspectSchema, makeRemoteExecutableSchema, mergeSchemas } from 'graphql-tools';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
 
 // ----------------------------------------------------------------------------
 
-export function createClient(): ApolloClient<NormalizedCacheObject> {
+export async function createClient(): Promise<any> {
   // Create the cache first, which we'll share across Apollo tooling.
   // This is an in-memory cache. Since we'll be calling `createClient` on
   // universally, the cache will survive until the HTTP request is
   // responded to (on the server) or for the whole of the user's visit (in
   // the browser)
   const cache = new InMemoryCache();
-
   // Create a HTTP client (both server/client). It takes the GraphQL
   // server from the `GRAPHQL` environment variable, which by default is
   // set to an external playground at https://graphqlhub.com/graphql
-  const httpLink = new HttpLink({
-    credentials: "same-origin",
-    uri: GRAPHQL
+  // const httpLink = new HttpLink({
+  //   credentials: "same-origin",
+  //   uri: GRAPHQL,
+  // });
+  //
+  // const httpLinkGraphene = new HttpLink({
+  //   credentials: "same-origin",
+  //   uri: GRAPHENE,
+  // });
+
+  // setup schema stitching
+  const getRemoteExecutableSchema = async (uri: string, creds: string) => {
+    const httpLink = new HttpLink({ uri, credentials: creds });
+    const remoteSchema = await introspectSchema(httpLink);
+    return makeRemoteExecutableSchema({ schema: remoteSchema, link: httpLink });
+  };
+  const executableSchema1 = await getRemoteExecutableSchema(GRAPHQL, 'same-origin');
+  const executableSchema2 = await getRemoteExecutableSchema(GRAPHENE, 'same-origin');
+
+  const newSchema = mergeSchemas({
+    schemas: [executableSchema1, executableSchema2],
   });
+
+  const link = new SchemaLink({ schema: newSchema });
+  // console.log('link is', link);
 
   // If we're in the browser, we'd have received initial state from the
   // server. Restore it, so the client app can continue with the same data.
@@ -40,7 +62,7 @@ export function createClient(): ApolloClient<NormalizedCacheObject> {
   // Return a new Apollo Client back, with the cache we've just created,
   // and an array of 'links' (Apollo parlance for GraphQL middleware)
   // to tell Apollo how to handle GraphQL requests
-  return new ApolloClient({
+  const c = new ApolloClient({
     cache,
     link: ApolloLink.from([
       // General error handler, to log errors back to the console.
@@ -52,8 +74,8 @@ export function createClient(): ApolloClient<NormalizedCacheObject> {
         if (graphQLErrors) {
           graphQLErrors.map(({ message, locations, path }) =>
             console.log(
-              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-            )
+              `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+            ),
           );
         }
         if (networkError) {
@@ -67,23 +89,24 @@ export function createClient(): ApolloClient<NormalizedCacheObject> {
             ({ query }) => {
               const definition = getMainDefinition(query);
               return (
-                definition.kind === "OperationDefinition" &&
-                definition.operation === "subscription"
+                definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
               );
             },
             // Use WebSockets for subscriptions
             new WebSocketLink(
               // Replace http(s) with `ws` for connecting via WebSockts
-              new SubscriptionClient(GRAPHQL.replace(/^https?/, "ws"), {
-                reconnect: true // <-- automatically redirect as needed
-              })
+              new SubscriptionClient(GRAPHQL.replace(/^https?/, 'ws'), {
+                reconnect: true, // <-- automatically redirect as needed
+              }),
             ),
             // ... fall-back to HTTP for everything else
-            httpLink
+            link,
           )
-        : httpLink // <-- just use HTTP on the server
+        : link, // <-- just use HTTP on the server
     ]),
     // On the server, enable SSR mode
-    ssrMode: SERVER
+    ssrMode: SERVER,
   });
+
+  return c;
 }
