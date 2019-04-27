9c902cf843d35551b80c49f512470209a0046513
andreas divaris <dredivaris@gmail.com>
2019-04-26 19:58:43 -0700

    Add login and registration graphql

--- src/components/bar/index.tsx
+++ src/components/bar/index.tsx
@@ -1,9 +1,19 @@
+import * as Cookies from 'js-cookie';
 import * as React from 'react';
+import { withRouter } from 'react-router';
 
 // ----------------------------------------------------------------------------
 
 // Say hello from GraphQL, along with a HackerNews feed fetched by GraphQL
-const Bar: React.FC = () => {
+const Bar: React.FC = ({history}) => {
+  let token = null;
+  try {
+    token = Cookies.get('homebarToken');
+  } catch {}
+  if (!token) {
+    history.push('/login');
+  }
+
   return (
     <>
       <h2>Bar loaded!</h2>
@@ -11,4 +21,4 @@ const Bar: React.FC = () => {
   );
 };
 
-export default Bar;
+export default withRouter(Bar);
