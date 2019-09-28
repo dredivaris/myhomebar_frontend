0d97355a88c81777008c7306ed8d7ceb83af4b03
andreas divaris <dredivaris@gmail.com>
2019-09-28 13:47:29 -0700

    Add initial state and change select

--- src/components/recipes/form.tsx
+++ src/components/recipes/form.tsx
@@ -1,3 +1,4 @@
+import { ADD_RECIPE } from '@/queries/graphene_graphql_mutations';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   Button,
@@ -9,11 +10,13 @@ import {
   MenuItem,
   Select,
   TextField,
-  Theme,
 } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/styles';
 import * as React from 'react';
+import Mutation from 'react-apollo/Mutation';
+import { Simulate } from 'react-dom/test-utils';
 import StarRatingComponent from 'react-star-rating-component';
+import input = Simulate.input;
 
 const drawerWidth = 1200;
 const useStyles = makeStyles((theme) => {
@@ -56,6 +59,7 @@ const useStyles = makeStyles((theme) => {
     },
     extras: {
       marginRight: '22px',
+      paddingTop: '6px',
     },
     grid: {
       padding: 8,
@@ -86,11 +90,31 @@ const useStyles = makeStyles((theme) => {
 });
 
 const RecipeForm: React.FC = ({}) => {
-  const theme: Theme = useTheme();
-
   const classes = useStyles();
   const [type, setType] = React.useState('COCKTAIL');
   const [rating, setRating] = React.useState(4);
+
+  const [values, setValues] = React.useState({
+    directions: '',
+    ingredients: '',
+    name: '',
+    rating: 0.0,
+    ratingSet: false,
+    recipeType: 'COCKTAIL',
+  });
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  const handleChangeSelect = (event) => {
+    console.log('in select change', event.target.name, event.target.value);
+
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const onStarClick = (nextValue, prevValue, name, e) => {
     const xPos =
@@ -102,187 +126,242 @@ const RecipeForm: React.FC = ({}) => {
 
     console.log('name: %s, nextValue: %s, prevValue: %s', name, nextValue, prevValue);
     // console.log(e);
-    setRating(nextValue);
+    setValues({ ...values, ['rating']: nextValue, ['ratingSet']: true });
   };
 
-
   return (
-    <form>
-      <Grid container direction="row">
-        <Grid item xs={12} sm={4} className={classes.grid}>
-          <FormControl margin="normal" required fullWidth>
-            <InputLabel htmlFor="email">Name</InputLabel>
-            <Input id="name" name="name" autoFocus />
-          </FormControl>
-          <FormControl margin="normal" fullWidth>
-            <FormLabel>Ingredients:</FormLabel>
-            <TextField
-              id="filled-multiline-static"
-              multiline
-              rows="35"
-              rowsMax="100"
-              defaultValue=""
-              className={classes.textField}
-              margin="normal"
-              variant="filled"
-            />
-          </FormControl>
-          <FormControl margin="normal" fullWidth>
-            <InputLabel htmlFor="source">Source</InputLabel>
-            <Input id="source" name="source" autoFocus className={classes.extras} />
-          </FormControl>
-          <FormControl margin="normal" fullWidth>
-            <InputLabel htmlFor="source-url">Source Url</InputLabel>
-            <Input id="source-url" name="source-url" autoFocus className={classes.extras} />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={8} className={classes.grid}>
-          <Grid container direction="row">
-            <Grid item xs={12} sm={8} className={classes.grid}>
-              <FormControl margin="dense" required fullWidth variant="filled">
-                <InputLabel>Type</InputLabel>
-                <Select
-                  value={type}
-                  // onChange={handleChange}
-                  // inputProps={{
-                  //   name: 'age',
-                  //   id: 'age-simple',
-                >
-                  <MenuItem value="COCKTAIL">
-                    <em>Cocktail</em>
-                  </MenuItem>
-                  <MenuItem value={'COCKTAIL'}>Cocktail</MenuItem>
-                  <MenuItem value={'CORDIAL'}>Cordial</MenuItem>
-                  <MenuItem value={'BITTERS'}>Bitters</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={4} className={classes.grid}>
-              <StarRatingComponent
-                name="app6"
-                starCount={5}
-                starColor="#ffb400"
-                emptyStarColor="#ffb400"
-                value={rating}
-                onStarClick={onStarClick}
-                renderStarIcon={(index: any, value: any) => {
-                  return (
-                    <span>
-                      <FontAwesomeIcon
-                        icon="star"
-                        size="lg"
-                        style={
-                          index <= value
-                            ? {
-                              color: 'rgb(255, 180, 0)',
-                              cursor: 'pointer',
-                              float: 'right',
-                            }
-                            : {
-                              color: 'rgb(51, 51, 51)',
-                              cursor: 'pointer',
-                              float: 'right',
-                            }
-                        }
+    <Mutation mutation={ADD_RECIPE}>
+      {(addRecipe, { data }) => (
+        <div>
+          <form
+            onSubmit={(e) => {
+              console.log('in onsubmit, ', values);
+
+              e.preventDefault();
+              addRecipe({
+                variables: values,
+              });
+              // nameInput.value = '';
+            }}
+          >
+            <Grid container direction="row">
+              <Grid item xs={12} sm={4} className={classes.grid}>
+                <FormControl margin="normal" required fullWidth>
+                  <InputLabel htmlFor="name">Name</InputLabel>
+                  <Input id="name" name="name" autoFocus onChange={handleChange('name')} />
+                </FormControl>
+                <FormControl margin="normal" required fullWidth>
+                  <FormLabel>Ingredients:</FormLabel>
+                  <TextField
+                    multiline
+                    rows="31"
+                    rowsMax="100"
+                    defaultValue=""
+                    className={classes.textField}
+                    margin="normal"
+                    variant="filled"
+                    id="ingredients"
+                    name="ingredients"
+                    onChange={handleChange('ingredients')}
+                  />
+                </FormControl>
+                <FormControl margin="normal" fullWidth>
+                  <InputLabel htmlFor="source">Source</InputLabel>
+                  <Input
+                    id="source"
+                    name="source"
+                    onChange={handleChange('source')}
+                    autoFocus
+                    className={classes.extras}
+                  />
+                </FormControl>
+                <FormControl margin="normal" fullWidth>
+                  <InputLabel htmlFor="source-url">Source Url</InputLabel>
+                  <Input
+                    id="source-url"
+                    name="source-url"
+                    type="url"
+                    onChange={handleChange('sourceUrl')}
+                    autoFocus
+                    className={classes.extras}
+                  />
+                </FormControl>
+                <FormControl margin="normal" fullWidth>
+                  <InputLabel htmlFor="attribution">Attribution</InputLabel>
+                  <Input
+                    id="attribution"
+                    name="attribution"
+                    onChange={handleChange('attribution')}
+                    autoFocus
+                    className={classes.extras}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12} sm={8} className={classes.grid}>
+                <Grid container direction="row">
+                  <Grid item xs={12} sm={8} className={classes.grid}>
+                    <FormControl margin="dense" required fullWidth variant="filled">
+                      <InputLabel>Type</InputLabel>
+                      <Select
+                        value={values.recipeType}
+                        onChange={handleChangeSelect}
+                        inputProps={{
+                          id: 'recipe-type',
+                          name: 'recipeType',
+                        }}
+                      >
+                        <MenuItem value="COCKTAIL">
+                          <em>Cocktail</em>
+                        </MenuItem>
+                        <MenuItem value={'COCKTAIL'}>Cocktail</MenuItem>
+                        <MenuItem value={'CORDIAL'}>Cordial</MenuItem>
+                        <MenuItem value={'BITTERS'}>Bitters</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={4} className={classes.grid}>
+                    <StarRatingComponent
+                      id="rating"
+                      name="rating"
+                      starCount={5}
+                      starColor="#ffb400"
+                      emptyStarColor="#ffb400"
+                      value={values.rating}
+                      onStarClick={onStarClick}
+                      renderStarIcon={(index: any, value: any) => {
+                        return (
+                          <span>
+                            <FontAwesomeIcon
+                              icon="star"
+                              size="lg"
+                              style={
+                                index <= value
+                                  ? {
+                                    color: 'rgb(255, 180, 0)',
+                                    cursor: 'pointer',
+                                    float: 'right',
+                                  }
+                                  : {
+                                    color: 'rgb(51, 51, 51)',
+                                    cursor: 'pointer',
+                                    float: 'right',
+                                  }
+                              }
+                            />
+                          </span>
+                        );
+                      }}
+                      renderStarIconHalf={() => {
+                        return (
+                          <span>
+                            <FontAwesomeIcon icon="star-half-alt" size="lg" />
+                          </span>
+                        );
+                      }}
+                    />
+                  </Grid>
+                  <Grid item xs={6} sm={4}>
+                    <FormControl margin="normal" fullWidth>
+                      <InputLabel htmlFor="glassware">Glassware</InputLabel>
+                      <Input
+                        id="glassware"
+                        name="glassware"
+                        onChange={handleChange('glassware')}
+                        autoFocus
+                        className={classes.extras}
                       />
-                    </span>
-                  );
-                }}
-                renderStarIconHalf={() => {
-                  // todo: add half star
-                  return (
-                    <span>
-                              <FontAwesomeIcon icon="star-half-alt" size="lg" />
-                            </span>
-                  );
-                }}
-              />
-            </Grid>
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6} sm={4}>
+                    <FormControl margin="normal" fullWidth>
+                      <InputLabel htmlFor="tools">Tools</InputLabel>
+                      <Input
+                        id="tools"
+                        name="tools"
+                        onChange={handleChange('tools')}
+                        autoFocus
+                        className={classes.extras}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={6} sm={4}>
+                    <FormControl margin="normal" fullWidth>
+                      <InputLabel htmlFor="garnish">Garnish</InputLabel>
+                      <Input
+                        id="garnish"
+                        name="garnish"
+                        onChange={handleChange('garnish')}
+                        autoFocus
+                        className={classes.extras}
+                      />
+                    </FormControl>
+                  </Grid>
 
-            <Grid item xs={6} sm={4}>
-              <FormControl margin="normal" fullWidth>
-                <InputLabel htmlFor="glassware">Glassware</InputLabel>
-                <Input
-                  id="glassware"
-                  name="glassware"
-                  autoFocus
-                  className={classes.extras}
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={4}>
-              <FormControl margin="normal" fullWidth>
-                <InputLabel htmlFor="tools">Tools</InputLabel>
-                <Input id="tools" name="tools" autoFocus className={classes.extras} />
-              </FormControl>
-            </Grid>
-            <Grid item xs={6} sm={4}>
-              <FormControl margin="normal" fullWidth>
-                <InputLabel htmlFor="garnish">Garnish</InputLabel>
-                <Input id="garnish" name="garnish" autoFocus className={classes.extras} />
-              </FormControl>
-            </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <FormControl margin="normal" fullWidth>
+                      <FormLabel>Description:</FormLabel>
+                      <TextField
+                        id="filled-multiline-static"
+                        multiline
+                        rows="8"
+                        rowsMax="100"
+                        defaultValue=""
+                        className={classes.textFieldRight}
+                        margin="normal"
+                        variant="filled"
+                        name="description"
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <FormControl margin="normal" required fullWidth>
+                      <FormLabel>Directions:</FormLabel>
+                      <TextField
+                        multiline
+                        rows="18"
+                        rowsMax="100"
+                        defaultValue=""
+                        className={classes.textFieldRight}
+                        margin="normal"
+                        variant="filled"
+                        id="directions"
+                        name="directions"
+                        onChange={handleChange('directions')}
+                      />
+                    </FormControl>
+                  </Grid>
+                  <Grid item xs={12} sm={12}>
+                    <FormControl margin="normal" fullWidth>
+                      <FormLabel>Notes:</FormLabel>
+                      <TextField
+                        id="notes"
+                        name="notes"
+                        onChange={handleChange('notes')}
+                        multiline
+                        rows="3"
+                        rowsMax="100"
+                        defaultValue=""
+                        className={classes.textFieldRight}
+                        margin="normal"
+                        variant="filled"
+                      />
+                    </FormControl>
+                  </Grid>
+                </Grid>
+              </Grid>
 
-            <Grid item xs={12} sm={12}>
-              <FormControl margin="normal" fullWidth>
-                <FormLabel>Description:</FormLabel>
-                <TextField
-                  id="filled-multiline-static"
-                  multiline
-                  rows="8"
-                  rowsMax="100"
-                  defaultValue=""
-                  className={classes.textFieldRight}
-                  margin="normal"
-                  variant="filled"
-                  name="description"
-                />
-              </FormControl>
+              {/*<FormControlLabel*/}
+              {/*  control={<Checkbox value="remember" color="primary" />}*/}
+              {/*  label="Remember me"*/}
+              {/*/>*/}
+              <Button type="submit" fullWidth variant="contained" color="primary">
+                Save recipe
+              </Button>
             </Grid>
-            <Grid item xs={12} sm={12}>
-              <FormControl margin="normal" fullWidth>
-                <FormLabel>Directions:</FormLabel>
-                <TextField
-                  id="filled-multiline-static"
-                  multiline
-                  rows="18"
-                  rowsMax="100"
-                  defaultValue=""
-                  className={classes.textFieldRight}
-                  margin="normal"
-                  variant="filled"
-                  name="directions"
-                />
-              </FormControl>
-            </Grid>
-            <Grid item xs={12} sm={12}>
-              <FormControl margin="normal" fullWidth>
-                <FormLabel>Notes:</FormLabel>
-                <TextField
-                  id="filled-multiline-static"
-                  multiline
-                  rows="3"
-                  rowsMax="100"
-                  defaultValue=""
-                  className={classes.textFieldRight}
-                  margin="normal"
-                  variant="filled"
-                  name="notes"
-                />
-              </FormControl>
-            </Grid>
-          </Grid>
-        </Grid>
-
-        {/*<FormControlLabel*/}
-        {/*  control={<Checkbox value="remember" color="primary" />}*/}
-        {/*  label="Remember me"*/}
-        {/*/>*/}
-        <Button type="submit" fullWidth variant="contained" color="primary">
-          Save recipe
-        </Button>
-      </Grid>
-    </form>
+          </form>
+        </div>
+      )}
+    </Mutation>
   );
 };
 
