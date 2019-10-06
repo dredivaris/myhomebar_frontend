3bb532cf4295688ad3452b00c4d4625ab95eaed0
andreas divaris <dredivaris@gmail.com>
2019-10-06 15:30:50 -0700

    Add main?

--- src/entry/client.tsx
+++ src/entry/client.tsx
@@ -6,36 +6,36 @@
 /* NPM */
 
 // Create browser history, for navigation a la single page apps
-import createBrowserHistory from "history/createBrowserHistory";
+import createBrowserHistory from 'history/createBrowserHistory';
 
 // React, our UI engine
-import * as React from "react";
+import * as React from 'react';
 
 // HOC for enabling Apollo GraphQL `<Query>` and `<Mutation>`
-import { ApolloProvider } from "react-apollo";
+import { ApolloProvider } from 'react-apollo';
 
 // Attach React to the browser DOM
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from 'react-dom';
 
 // Single page app routing
-import { Router } from "react-router-dom";
+import { Router } from 'react-router-dom';
 
 /* Local */
 
 // Our main component, and the starting point for server/browser loading
-import Root from "@/components/root";
+import Root from '@/components/root';
 
-// Helper function that creates a new Apollo client per request
-import { createClient } from "@/lib/apollo";
+// Helper function that cretes a new Apollo client per request
+import { createClient } from '@/lib/apollo';
 
 // MobX state
-import { State } from "@/data/state";
-import { rehydrate, StateProvider } from "@/lib/mobx";
+import { State } from '@/data/state';
+import { rehydrate, StateProvider } from '@/lib/mobx';
+import { CookiesProvider } from 'react-cookie';
 
 // ----------------------------------------------------------------------------
 
 // Create Apollo client
-const client = createClient();
 
 // Create new MobX state
 const state = new State();
@@ -47,13 +47,31 @@ const history = createBrowserHistory();
 rehydrate(state);
 
 // Render
-ReactDOM.hydrate(
-  <StateProvider value={state}>
-    <ApolloProvider client={client}>
-      <Router history={history}>
-        <Root />
-      </Router>
-    </ApolloProvider>
-  </StateProvider>,
-  document.getElementById("root")
-);
+class Main extends React.Component {
+  state = {
+    client: null,
+  };
+  async componentWillMount() {
+    try {
+      const client = await createClient(null);
+      this.setState({ client });
+    } catch (e) {
+      console.log('Client failed with', e);
+    }
+  }
+
+  render() {
+    return (
+      <CookiesProvider>
+        <StateProvider value={state}>
+          <ApolloProvider client={this.state.client!}>
+            <Router history={history}>
+              <Root />
+            </Router>
+          </ApolloProvider>
+        </StateProvider>
+      </CookiesProvider>
+    );
+  }
+}
+ReactDOM.render(<Main />, document.getElementById('root'));
