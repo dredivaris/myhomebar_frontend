c9f8573f4d2767c278708686ceaf53cec3191a17
andreas divaris <dredivaris@gmail.com>
2019-10-06 15:35:11 -0700

    Update with theme?

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -5,15 +5,16 @@
 
 /* NPM */
 import { Global } from '@emotion/core';
+import { createMuiTheme } from '@material-ui/core/styles';
 import * as React from 'react';
 import Helmet from 'react-helmet';
 import { hot } from 'react-hot-loader';
 import { Route, Switch } from 'react-router-dom';
-import { createMuiTheme } from "@material-ui/core/styles";
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+
 library.add(faStarHalfAlt);
 library.add(faStar);
 
@@ -33,22 +34,24 @@ import ThemeProvider from '@material-ui/styles/ThemeProvider';
 // ----------------------------------------------------------------------------
 const theme = createMuiTheme({});
 
-const Root = () => (
-  <ThemeProvider theme={theme}>
-    <Global styles={globalStyles} />
-    <Helmet>
-      <title>HomeBar</title>
-    </Helmet>
-    <ScrollTop>
-      <Switch>
-        {routes.map((route) => (
-          <Route key={route.path} {...route} />
-        ))}
-      </Switch>
-    </ScrollTop>
-    <NavBar />
-  </ThemeProvider>
-);
+const Root = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <Helmet>
+        <title>HomeBar</title>
+      </Helmet>
+      <ScrollTop>
+        <Switch>
+          {routes.map((route) => (
+            <Route key={route.path} {...route} />
+          ))}
+        </Switch>
+      </ScrollTop>
+      <NavBar />
+    </ThemeProvider>
+  );
+};
 
 export default hot(module)(Root);
 
