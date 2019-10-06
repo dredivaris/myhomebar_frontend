93e097cf2721995e817cf4f5b2ca34de61e9805f
andreas divaris <dredivaris@gmail.com>
2019-10-06 15:36:13 -0700

    Update to apollo setup

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -6,18 +6,22 @@
 /* NPM */
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
-import { ApolloLink, split } from 'apollo-link';
+import { ApolloLink } from 'apollo-link';
+import { setContext } from 'apollo-link-context';
 import { onError } from 'apollo-link-error';
-import { HttpLink } from 'apollo-link-http';
+import { HttpLink, createHttpLink } from 'apollo-link-http';
 import { SchemaLink } from 'apollo-link-schema';
 import { WebSocketLink } from 'apollo-link-ws';
 import { getMainDefinition } from 'apollo-utilities';
 import { introspectSchema, makeRemoteExecutableSchema, mergeSchemas } from 'graphql-tools';
+// import * as Cookies from 'js-cookie';
+import Cookies from 'universal-cookie';
+import * as cookie from 'cookie';
 import { SubscriptionClient } from 'subscriptions-transport-ws';
 
 // ----------------------------------------------------------------------------
 
-export async function createClient(): Promise<any> {
+export async function createClient(externalCookies): Promise<any> {
   // Create the cache first, which we'll share across Apollo tooling.
   // This is an in-memory cache. Since we'll be calling `createClient` on
   // universally, the cache will survive until the HTTP request is
@@ -36,10 +40,30 @@ export async function createClient(): Promise<any> {
   //   credentials: "same-origin",
   //   uri: GRAPHENE,
   // });
+  let token: string | null | undefined;
+  let cookies = new Cookies();
+
+  try {
+
+    if (!token) {
+      token = cookies.get('homebarToken');
+      if (token === undefined) {
+        const cookieList = cookie.parse(externalCookies);
+        token = cookieList.homebarToken;
+      }
+    }
+  } catch (e) {
+  }
 
   // setup schema stitching
   const getRemoteExecutableSchema = async (uri: string, creds: string) => {
-    const httpLink = new HttpLink({ uri, credentials: creds });
+    const httpLink = new HttpLink({
+      uri,
+      credentials: creds,
+      headers: {
+        'content-type': 'application/json',
+        'authorization': token ? `JWT ${token}` : '',
+      }});
     const remoteSchema = await introspectSchema(httpLink);
     return makeRemoteExecutableSchema({ schema: remoteSchema, link: httpLink });
   };
@@ -50,21 +74,27 @@ export async function createClient(): Promise<any> {
     schemas: [executableSchema1, executableSchema2],
   });
 
-  const link = new SchemaLink({ schema: newSchema });
-  // console.log('link is', link);
-
   // If we're in the browser, we'd have received initial state from the
   // server. Restore it, so the client app can continue with the same data.
   if (!SERVER) {
+    console.log('not in server.. in browser');
     cache.restore((window as any).__APOLLO_STATE__);
   }
+
+  const link = new SchemaLink({ schema: newSchema });
+  // const httpLink = createHttpLink({ headers, });
+  // const setAuthorizationLink = setContext((request, previousContext) => ({
+  //   headers: {authorization: token ? `JWT ${ token }` : 'blah' }
+  // }));
 
   // Return a new Apollo Client back, with the cache we've just created,
   // and an array of 'links' (Apollo parlance for GraphQL middleware)
   // to tell Apollo how to handle GraphQL requests
+
   const c = new ApolloClient({
     cache,
     link: ApolloLink.from([
+      link,
       // General error handler, to log errors back to the console.
       // Replace this in production with whatever makes sense in your
       // environment. Remember you can use the global `SERVER` variable to
@@ -82,27 +112,6 @@ export async function createClient(): Promise<any> {
           console.log(`[Network error]: ${networkError}`);
         }
       }),
-
-      // Split on HTTP and WebSockets
-      WS_SUBSCRIPTIONS && !SERVER
-        ? split(
-            ({ query }) => {
-              const definition = getMainDefinition(query);
-              return (
-                definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-              );
-            },
-            // Use WebSockets for subscriptions
-            new WebSocketLink(
-              // Replace http(s) with `ws` for connecting via WebSockts
-              new SubscriptionClient(GRAPHQL.replace(/^https?/, 'ws'), {
-                reconnect: true, // <-- automatically redirect as needed
-              }),
-            ),
-            // ... fall-back to HTTP for everything else
-            link,
-          )
-        : link, // <-- just use HTTP on the server
     ]),
     // On the server, enable SSR mode
     ssrMode: SERVER,
