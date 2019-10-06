dbab10a63c0e14a84a74196597f887b281a4c536
andreas divaris <dredivaris@gmail.com>
2019-10-06 16:10:56 -0700

    Add basic filter functions

--- src/components/recipes/recipes.tsx
+++ src/components/recipes/recipes.tsx
@@ -1,14 +1,13 @@
 import Recipe from '@/components/recipes/recipe';
 import { USERS_RECIPES_FULL } from '@/queries/graphene_graphql_queries';
-import { List, Typography } from '@material-ui/core';
 import * as React from 'react';
 import { Query } from 'react-apollo';
-
 // ----------------------------------------------------------------------------
 
 // Say hello from GraphQL, along with a HackerNews feed fetched by GraphQL
-const Recipes: React.FC = () => {
+const Recipes: React.FC = (props) => {
   const [expanded, setExpanded] = React.useState(false);
+  const { filterText } = props;
 
   const handleChange = (panel: any) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -17,10 +16,6 @@ const Recipes: React.FC = () => {
   return (
     <Query query={USERS_RECIPES_FULL}>
       {(result) => {
-        // if (result.data && result.data.usersRecipes) {
-        //   const beverages = result.data.usersRecipes;
-        // }
-
         // Any errors? Say so!
         if (result.error) {
           return <h1>Error retrieving units! &mdash; {result.error}</h1>;
@@ -31,22 +26,30 @@ const Recipes: React.FC = () => {
         if (result.loading) {
           return <h1>Loading units ...</h1>;
         }
-        // const name = result.data!.allApiUnits!.nodes[0]!.name;
-        // const {
-        //   data: {
-        //     allApiUnits: { nodes },
-        //   },
-        // } = result;
 
         // Otherwise, we have data to work with... map over it with a
         // bullet-point list
-
+        function inName(recipe, text) {
+          return recipe.name.toLowerCase().includes(text);
+        }
+        function inIngredient(recipe, text) {
+          for (const ingredient of recipe.ingredients) {
+            if (ingredient.toLowerCase().includes(text)) {
+              return true;
+            }
+          }
+          return false;
+        }
+        const recipesFiltered = result.data.usersRecipes.filter((recipe: any) => {
+          if (filterText.length > 0) {
+            let text = filterText.toLowerCase();
+            return inName(recipe, text) || inIngredient(recipe, text);
+          } else return true;
+        });
         return (
           <>
             <div>
-              {result.data &&
-                result.data.usersRecipes &&
-                result.data.usersRecipes.map((recipe: any) => {
+              {recipesFiltered.map((recipe: any) => {
                   return (
                     <Recipe usersRecipe={recipe} handleChange={handleChange} expanded={expanded} />
                   );
